b4cd110c1f3d4c066e46c21f72aed52a1836291b
simonastojanovska21 <70956775+simonastojanovska21@users.noreply.github.com>
2021-08-12 23:15:00 +0200

    project structure

---
+++ ExamManagerApplication/ExamManager/ExamManager.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExamManager.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ ExamManagerApplication/ExamManager/ExamManager.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExamManager.Service
+{
+    public class Class1
+    {
+    }
+}
